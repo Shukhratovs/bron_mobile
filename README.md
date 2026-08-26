f4a03c00db4f76d9a04d59a5615dd91868ad19ec
Zoyidov <nurmuxammadzoyidov@gmail.com>
2026-08-26 10:27:51 +0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# bron_mobile
+
+A new Flutter project.
---
+++ android/app/src/main/kotlin/bron/mobile/uz/bron_mobile/MainActivity.kt
@@ -0,0 +1,5 @@
+package bron.mobile.uz.bron_mobile
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
